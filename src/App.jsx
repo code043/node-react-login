9d90127934da42e4723dc8e9e4143eb9523866f6
code043 <procoder043@gmail.com>
2024-10-20 19:15:17 -0300

    Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import Register from './Register'
-import Login from './Login'
-import Forgot from './Forgot'
-import ResetPassword from './ResetPassword';
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+import Forgot from "./Forgot";
+import ResetPassword from "./ResetPassword";
 
 function App() {
-
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/forgot-password' element={<Forgot/>}></Route>
-      <Route path='/reset-password/:id/:token' element={<ResetPassword/>}></Route>
-    </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/forgot-password" element={<Forgot />}></Route>
+        <Route
+          path="/reset-password/:id/:token"
+          element={<ResetPassword />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
